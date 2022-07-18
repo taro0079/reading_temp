143bf06a2fe08f54f130a3e55de228743976d711
Taro Morita <morita0079@gmail.com>
2022-07-19 01:15:21 +0900

    温度を取得

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::fs;
+extern crate regex;
+use regex::Regex;
 fn main() {
     let file = fs::read_to_string("/sys/bus/w1/devices/28-3c01f0952927/w1_slave").unwrap();
-    println!("{}", file);
+    let re = Regex::new(r"t=\d+").unwrap();
+    let caps = re.captures(&file).unwrap();
+    let temp = caps.at(0).unwrap().replace("t=", "");
+    let temp = temp.parse::<f64>().unwrap() / 1000.;
+    println!("{}", temp);
 }
